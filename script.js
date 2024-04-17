ac2c5e7c6a2c4a173991c360dfbd282124f86ea1
ChaseCushman <161781064+ChaseCushman@users.noreply.github.com>
2024-04-16 21:24:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://uy5j64sv72.execute-api.us-east-2.amazonaws.com/dev/tasks';
+const API_URL = 'https://t85uqonty0.execute-api.us-east-2.amazonaws.com/dev';
 
 // Function to fetch tasks from API and render them
 async function fetchTasks() {
