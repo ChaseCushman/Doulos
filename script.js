f3bf8b3a82ccb87442797e600eeb0dbfa9239051
ChaseCushman <161781064+ChaseCushman@users.noreply.github.com>
2024-04-17 20:32:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const API_URL = 'https://j4cbe0afa0.execute-api.us-east-2.amazonaws.com/dev';
 // Function to fetch tasks from API and render them
 async function fetchTasks() {
     try {
-        const response = await fetch(`${API_URL}?TableName=DoulosDB`);
+        const response = await fetch(`${API_URL}?TableName=DoulosDB`, {
+            headers: {
+                'mode': 'no-cors'
+            }});
         const data = await response.json();
         renderTasks(data.Items);
     } catch (error) {
