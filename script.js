e17672fa6a0b7f7c8531b0339d84f641deb3ae81
ChaseCushman <161781064+ChaseCushman@users.noreply.github.com>
2024-04-17 21:23:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const API_URL = 'https://j4cbe0afa0.execute-api.us-east-2.amazonaws.com/dev';
 // Function to fetch tasks from API and render them
 async function fetchTasks() {
     try {
-        const response = await fetch(`${API_URL}`);
+        const response = await fetch(`https://j4cbe0afa0.execute-api.us-east-2.amazonaws.com/dev`);
         const data = await response.json();
         renderTasks(data.Items);
     } catch (error) {
